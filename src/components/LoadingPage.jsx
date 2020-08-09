be925584f28c616267dfda3fa0931b9c3783c652
SwagarikaGiri <swagaarika95@gmail.com>
2020-08-09 20:43:49 +0530

    changes made in loading page

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -5,7 +5,7 @@ class LoadingPage extends React.Component {
     render(){
         return(
             <div>
-                <h5  className="section-subheading">Please wait for some to get the results </h5>
+                <h5  className="section-subheading">Please wait for some time to get the results </h5>
             </div>
         )
     }
