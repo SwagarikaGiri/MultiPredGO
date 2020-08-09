7e03f6c9664cf9cf764275319d9b789fece6b2ca
SwagarikaGiri <swagaarika95@gmail.com>
2020-08-09 12:01:00 +0530

    Updates

---
+++ precache-manifest.e5806b4fec69bb12b6bf284a62f7595f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f4ee9a9e6db0aca095c42ef7a555f5",
+    "url": "/MultiPredGO/index.html"
+  },
+  {
+    "revision": "8e054b37c95b1a41b4d5",
+    "url": "/MultiPredGO/static/css/main.be68518a.chunk.css"
+  },
+  {
+    "revision": "dcbc686a905231f7b5c3",
+    "url": "/MultiPredGO/static/js/2.7ca24509.chunk.js"
+  },
+  {
+    "revision": "8e054b37c95b1a41b4d5",
+    "url": "/MultiPredGO/static/js/main.c3ba3600.chunk.js"
+  },
+  {
+    "revision": "21658d005f61538be116",
+    "url": "/MultiPredGO/static/js/runtime~main.64338d74.js"
+  },
+  {
+    "revision": "2f342a353e72c982e4645169b49f54ce",
+    "url": "/MultiPredGO/static/media/1.2f342a35.jpeg"
+  },
+  {
+    "revision": "ac862bb813ae9b0048a976cb966b4035",
+    "url": "/MultiPredGO/static/media/2.ac862bb8.jpeg"
+  },
+  {
+    "revision": "2ac45e9dfaf52d06423269cfd17b012e",
+    "url": "/MultiPredGO/static/media/4.2ac45e9d.jpeg"
+  },
+  {
+    "revision": "a688d468043df0f42c7e4b9a722c2fe2",
+    "url": "/MultiPredGO/static/media/header-bg.a688d468.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/MultiPredGO/static/media/map-image.f40ed5b1.png"
+  }
+]);
